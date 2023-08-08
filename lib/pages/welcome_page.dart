ac868a29ed4e0182d99dcfa31b56d4fef95e16c7
ehteshamawan1 <ehtesham.awan.ea@gmail.com>
2023-08-08 19:21:09 +0500

    committed

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -8,11 +8,30 @@ class WelcomePage extends StatefulWidget {
 }
 
 class _WelcomePageState extends State<WelcomePage> {
+  List images = [
+    "welcome-one.png",
+    "welcome-two.png",
+    "welcome-three.png",
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView.builder(itemBuilder: (_, index){
-
+      body: PageView.builder(
+          scrollDirection: Axis.vertical,
+          itemCount: images.length,
+          itemBuilder: (_, index){
+            return Container(
+              width: double.maxFinite,
+              height: double.maxFinite,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    "img/$images[index]"
+                  ),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            );
       }),
     );
   }
